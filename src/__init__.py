dc776e6b823f4d5becdeced96f0a7a78ea02c01f
Haphior <nikomarambio11@gmail.com>
2024-06-02 14:11:56 -0400

    cambios a init

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# src/__init__.py
+
+from .modules import administrador_redes, campus, dispositivo
+
+__all__ = ['administrador_redes', 'campus', 'dispositivo']
---
+++ src/modules/__init__.py
@@ -0,0 +1,7 @@
+# src/modules/__init__.py
+
+from .administrador_redes import AdministradorRedes
+from .campus import Campus
+from .dispositivo import Dispositivo
+
+__all__ = ['AdministradorRedes', 'Campus', 'Dispositivo']
